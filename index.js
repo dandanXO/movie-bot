671ea93d37a9d021069e6ed835be2c565684ed14
dandanXO <pigpig850130@gmail.com>
2019-03-31 14:52:41 +0800

    add ssl to express

--- index.js
+++ index.js
@@ -3,21 +3,43 @@ const linebot = require('linebot'),
 
 const lineSecret = require('./config/linceSecret.json');
 const util = require('util');
+const https = require('https');
+const fs = require('fs');
+var keyPath = './config/private.key';
+var certPath = './config/certificate.pem';
+var hskey = fs.readFileSync(keyPath);
+var hscert = fs.readFileSync(certPath);
 
 let bot = linebot({
     channelId: lineSecret.channelId,
     channelSecret: lineSecret.channelSecret,
     channelAccessToken: lineSecret.channelAccessToken
 });
-const linebotParser = bot.parser(),
-    app = express();
+
+
+const linebotParser = bot.parser();
+const  app = express();
 bot.on('message', function (event) {
     // 把收到訊息的 event 印出來
     console.log(event);
 });
+
 app.post('/webhook', linebotParser);
-// 在 localhost 走 8080 port
-let server = app.listen(process.env.PORT || 8080, function () {
-    let port = server.address().port;
-    console.log("My Line bot App running on port", port);
-});
+app.get('/test',(req, res)=>{
+    res.send('ssl test')
+})
+// 在 localhost 走 ssl 443 port
+
+const server = https.createServer({
+    key: hskey,
+    cert:hscert
+},app)
+server.listen(process.env.PORT || 443, '192.168.0.100',()=>{
+    console.log(`server on https://192.168.0.100`)
+})
+
+
+// let server = app.listen(process.env.PORT || 443, function () {
+//     let port = server.address().port;
+//     console.log("My Line bot App running on port", port);
+// });
