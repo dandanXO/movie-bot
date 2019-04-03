d28a92b6b83f956feab45f724edfa0b4fdc11c00
franklin <q35010013@gmail.com>
2019-04-03 17:41:46 +0800

    Refactoring some variable

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const linebot = require('linebot'),
-    express = require('express');
+    express = require('express'),
+    util = require('util'),
+    https = require('https'),
+    fs = require('fs');
 
 const lineSecret = require('./config/linceSecret.json');
-const util = require('util');
-const https = require('https');
-const fs = require('fs');
-var keyPath = './config/private.key';
-var certPath = './config/certificate.pem';
-var caPath = './config/ca_bundle.pem'
-var hskey = fs.readFileSync(keyPath);
-var hscert = fs.readFileSync(certPath);
-var hsca = fs.readFileSync(caPath, 'utf8')
+
+const PATH_PRIVATE_KEY = './config/private.key';
+const PATH_CERTIFICATE = './config/certificate.pem';
+const PATH_CA_BUNDLE = './config/ca_bundle.pem'
+const hskey = fs.readFileSync(PATH_PRIVATE_KEY);
+const hscert = fs.readFileSync(PATH_CERTIFICATE);
+const hsca = fs.readFileSync(PATH_CA_BUNDLE, 'utf8');
 
 let bot = linebot({
     channelId: lineSecret.channelId,
@@ -20,15 +21,15 @@ let bot = linebot({
 const linebotParser = bot.parser();
 const app = express();
 
-const getLatestMovie = require('./feature/getLatestMovieData')
-const getMovieLocalTherterAndTime = require('./feature/getMovieLocalTherterAndTime')
-
+//Import function
+const getLatestMovie = require('./feature/getLatestMovieData');
+const getMovieLocalTherterAndTime = require('./feature/getMovieLocalTherterAndTime');
 
 bot.on('message', function (event) {
     const askLatestMovie = event.message.text.match(/最新電影/);
-    const movielocal = event.message.text.match(/場次|地點/)
+    const movielocal = event.message.text.match(/場次|地點/);
     if (movielocal) {
-        const messages = event.message.text.split(' ')
+        const messages = event.message.text.split(' ');
         getMovieLocalTherterAndTime.getMovieLocalTherterAndTime(messages[1], messages[2])
             .then(data => {
                 let replays = [`久等了~ 使用方式 E.g: 場次 {電影名稱} {縣市名稱}`]
@@ -42,32 +43,40 @@ bot.on('message', function (event) {
                         for (let k = 0; k < forloop; k++) {
                             replay = ''
                             for (let i = k * 40; i < 40 * (k + 1); i++) {
-                                replay =replay+ `時間: ${data[i].time} ,地點:${data[i].theater}\n`
+                                replay = replay + `時間: ${data[i].time} ,地點:${data[i].theater}\n`
                             }
                             replays.push(replay)
                         }
                         event.reply(replays)
-                            .then(() => { })
-                            .catch((e) => { console.log(e) })
+                            .then(() => {})
+                            .catch((e) => {
+                                console.log(e)
+                            })
                     } else {
                         for (let i = 0; i < data.length; i++) {
                             replay = replay + `時間: ${data[i].time} ,地點:${data[i].theater}\n`
                         }
                         replays.push(replay)
                         event.reply(replays)
-                            .then(() => { })
-                            .catch((e) => { console.log(e) })
+                            .then(() => {})
+                            .catch((e) => {
+                                console.log(e)
+                            })
                     }
 
                 } else {
                     replays.push('QQ 沒東西')
                     event.reply(replays)
-                        .then(() => { })
-                        .catch((e) => { console.log(e) })
+                        .then(() => {})
+                        .catch((e) => {
+                            console.log(e)
+                        });
                 }
 
             })
-            .catch(e => { console.log(e) })
+            .catch(e => {
+                console.log(e)
+            })
 
     }
     if (askLatestMovie) {
@@ -81,17 +90,20 @@ bot.on('message', function (event) {
                 for (let k = 0; k < forloop; k++) {
                     let reply = ''
                     for (let i = k * 15; i < 15 * (k + 1); i++) {
-                        reply = reply + `
-電影名稱: ${data[i].name}
-電影詳細介紹: ${data[i].introductionUrl}
-電影預告片: ${data[i].youtubeUrl}
-`
+                        reply = reply +
+                            `電影名稱: ${data[i].name}` +
+                            `電影詳細介紹: ${data[i].introductionUrl}` +
+                            `電影預告片: ${data[i].youtubeUrl}`;
                     }
-                    replys.push(reply)
+                    replys.push(reply);
                 }
                 replys.push('最新的在最上面，資料有點多請見諒囉')
-                event.reply(replys).catch(e => { console.log(e) })
-            }).catch(e => { console.log(e) })
+                event.reply(replys).catch(e => {
+                    console.log(e)
+                });
+            }).catch(e => {
+                console.log(e)
+            })
     }
 
 });
@@ -104,9 +116,7 @@ const server = https.createServer({
     ca: hsca,
     key: hskey,
     cert: hscert
-}, app)
+}, app);
 server.listen(process.env.PORT || 443, '192.168.0.100', () => {
     console.log(`server on https://192.168.0.100`)
-})
-
-
+});
